2d7a4e1bd16ff61548350983053d105ace85ef0d
nivethini <mvnivethini@gmail.com>
2016-02-10 12:33:08 +0530

    add

---
+++ app/app/views/products/constant.js
@@ -0,0 +1,4 @@
+https://api.onedrive.com/v1.0
+
+var APP_CLIENT_ID = "00000000401829FB";
+var REDIRECT_URL = "http://localhost:3000/articles/index";
---
+++ app/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'products/index'
+
+  get 'articles/index'
+root 'articles#index'
+
+get 'product/callback'
+get 'articles/wl'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Serve websocket cable requests in-process
+  # mount ActionCable.server => '/cable'
+end
